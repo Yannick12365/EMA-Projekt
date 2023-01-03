bffd341dbeced91f4cd4015fc47930d846bd65d5
Yannick12365 <mcmasterdebabo@gmail.com>
2023-01-03 13:58:54 +0100

    add Login Screen add Item for Vorratskammer

--- app/src/main/java/com/example/ema_projekt/MainActivity.kt
+++ app/src/main/java/com/example/ema_projekt/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.ema_projekt
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.ema_projekt.kalender.KalenderActivity
+import com.example.ema_projekt.wgplaner.WGPlanerActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button:Button = findViewById(R.id.button_einlogen)
+
+        button.setOnClickListener {
+            startActivity(Intent(this, WGPlanerActivity::class.java))
+        }
     }
 }
