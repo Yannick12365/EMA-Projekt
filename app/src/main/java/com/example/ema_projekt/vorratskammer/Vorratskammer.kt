8c8b52c3835658f8a56b27d1fb156cb7f1f240db
Jannis Bressan <jannisbressan@web.de>
2023-01-04 14:56:27 +0100

    .

--- app/src/main/java/com/example/ema_projekt/vorratskammer/Vorratskammer.kt
+++ app/src/main/java/com/example/ema_projekt/vorratskammer/Vorratskammer.kt
@@ -1,20 +1,25 @@
 package com.example.ema_projekt.vorratskammer
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.DefaultItemAnimator
+import android.widget.*
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.ema_projekt.R
 
-class MainActivity : AppCompatActivity() {
+class Vorratskammer : AppCompatActivity() {
     private val itemsList = ArrayList<String>()
     private lateinit var customAdapter: CustomAdapter
+    private lateinit var erstellen: com.google.android.material.floatingactionbutton.FloatingActionButton;
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_vorratskammer)
-        title = "Vorratskammer"
 
         val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
         customAdapter = CustomAdapter(itemsList)
@@ -22,6 +27,53 @@ class MainActivity : AppCompatActivity() {
         recyclerView.layoutManager = layoutManager
         recyclerView.adapter = customAdapter
         prepareItems()
+
+        erstellen = findViewById(R.id.add)
+
+        erstellen.setOnClickListener{
+           showEventAddPopUp()
+        }
+
+    }
+   private fun showEventAddPopUp(){
+        val eventPopUp = Dialog(this)
+        var neu: String
+
+        eventPopUp.setContentView(R.layout.popup_vorratskammer_add)
+        eventPopUp.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+
+
+        val zurueckAdd: ImageButton = eventPopUp.findViewById(R.id.imageButton_vorratskammer_zurueck)
+        val abbrechen: Button = eventPopUp.findViewById(R.id.button_vorratskammer_abbrechen)
+        val hinzufuegen: Button = eventPopUp.findViewById(R.id.button_vorratskammer_hinzufuegen)
+        val eventText: EditText = eventPopUp.findViewById(R.id.editText_Neu)
+
+
+        zurueckAdd.setOnClickListener {
+            zurueckAdd.setBackgroundResource(R.drawable.zurueckklick)
+            eventPopUp.dismiss()
+        }
+        abbrechen.setOnClickListener {
+            eventPopUp.dismiss()
+        }
+
+        hinzufuegen.setOnClickListener {
+            if (eventText.text.isNotEmpty()) {
+                neu = eventText.text.toString()
+                itemsList.add(neu)
+                customAdapter.notifyDataSetChanged()
+                eventPopUp.dismiss()
+            } else{
+                val toast = Toast.makeText(applicationContext, "Gebe einen Text ein!", Toast.LENGTH_SHORT)
+                toast.show()
+            }
+        }
+
+
+
+
+        eventPopUp.show()
     }
     private fun prepareItems() {
         itemsList.add("Item 1")
---
+++ app/src/main/java/com/example/ema_projekt/vorratskammer/neu_vorratskammer.kt
@@ -0,0 +1,5 @@
+package com.example.ema_projekt.vorratskammer
+
+data class neu_vorratskammer(
+    var objekt: String
+)
