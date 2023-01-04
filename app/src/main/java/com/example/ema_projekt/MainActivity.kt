90e6ac64eb3b733d6458daae35e5d04de730df8d
Yannick12365 <mcmasterdebabo@gmail.com>
2023-01-04 14:26:34 +0100

    fix dark mode

--- app/src/main/java/com/example/ema_projekt/MainActivity.kt
+++ app/src/main/java/com/example/ema_projekt/MainActivity.kt
@@ -4,12 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AppCompatDelegate
 import com.example.ema_projekt.kalender.KalenderActivity
 import com.example.ema_projekt.wgplaner.WGPlanerActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         setContentView(R.layout.activity_main)
 
         val button:Button = findViewById(R.id.button_einlogen)
