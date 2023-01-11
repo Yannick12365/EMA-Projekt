1c9eee8703a195e2288071dc675d6b9bdc38749b
Yannick12365 <mcmasterdebabo@gmail.com>
2023-01-11 13:23:40 +0100

    edit MainActivity.kt add Database connect with createAccount + Login

--- app/src/main/java/com/example/ema_projekt/MainActivity.kt
+++ app/src/main/java/com/example/ema_projekt/MainActivity.kt
@@ -6,19 +6,28 @@ import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatDelegate
 import com.example.ema_projekt.kalender.KalenderActivity
 import com.example.ema_projekt.wgplaner.WGPlanerActivity
+import com.google.firebase.database.*
+import com.google.firebase.database.ktx.childEvents
 
 
 //https://youtu.be/kMEkP6f9_kE
 
 
 class MainActivity : AppCompatActivity() {
+    val database: DatabaseReference = FirebaseDatabase.getInstance("https://ema-projekt-e036e-default-rtdb.europe-west1.firebasedatabase.app/").reference
+
+    private lateinit var editTextName:EditText
+    private lateinit var editTextToken:EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
@@ -26,9 +35,30 @@ class MainActivity : AppCompatActivity() {
 
         val loginbutton:Button = findViewById(R.id.button_einloggen)
         val erstellenTextview:TextView = findViewById(R.id.textView_wgerstellen)
+        editTextName = findViewById(R.id.wgnameinput)
+        editTextToken = findViewById(R.id.wgtokeninput)
 
         loginbutton.setOnClickListener {
-            startActivity(Intent(this, WGPlanerActivity::class.java))
+            database.addListenerForSingleValueEvent(object : ValueEventListener{
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    if (dataSnapshot.hasChild(editTextName.text.toString())){
+                        if (dataSnapshot.child(editTextName.text.toString()).child("token").value.toString() == editTextToken.text.toString()){
+                            startActivity(Intent(this@MainActivity, WGPlanerActivity::class.java))
+                        } else {
+                            Toast.makeText(applicationContext, "Angegebener Token ist falsch!",
+                                Toast.LENGTH_SHORT).show()
+                        }
+                    } else {
+                        Toast.makeText(applicationContext, "Diese WG existiert nicht!",
+                            Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    Toast.makeText(applicationContext, "Ups, da ist etwas schief gelaufen!",
+                        Toast.LENGTH_SHORT).show()
+                }
+            })
         }
 
         erstellenTextview.setOnClickListener {
@@ -46,11 +76,6 @@ class MainActivity : AppCompatActivity() {
         val bestaetigen: Button = popup.findViewById(R.id.button_wgerstellen_bestaetigen)
         val abbrechen:Button = popup.findViewById(R.id.button_wgerstellen_abbrechen)
         val textfield: EditText = popup.findViewById(R.id.editText_wgname)
-        val errorfield: TextView = popup.findViewById(R.id.textView_error)
-
-        textfield.setOnClickListener {
-            errorfield.text = ""
-        }
 
         zurueck.setOnClickListener {
             popup.dismiss()
@@ -62,12 +87,47 @@ class MainActivity : AppCompatActivity() {
 
         bestaetigen.setOnClickListener {
             if (!textfield.text.toString().contains(" ")){
-                popup.dismiss()
+                if (textfield.text.toString().isNotEmpty()) {
+                    val wgname = textfield.text.toString().lowercase()
+
+                    database.addListenerForSingleValueEvent(object : ValueEventListener {
+                        override fun onDataChange(dataSnapshot: DataSnapshot) {
+                            if (!dataSnapshot.hasChild(wgname)) {
+                                val zeichen = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+
+                                var token = "$wgname-"
+                                for (i in 1..5) {
+                                    token += zeichen.random()
+                                }
+                                database.child(wgname).child("token").setValue(token)
+                                Toast.makeText(applicationContext,
+                                    "WG erfolgreich erstellt!",
+                                    Toast.LENGTH_SHORT).show()
+
+                                editTextName.setText(wgname)
+                                editTextToken.setText(token)
+                                popup.dismiss()
+                            } else {
+                                Toast.makeText(applicationContext,
+                                    "Der angegebene WG Name existiert schon!",
+                                    Toast.LENGTH_SHORT).show()
+                            }
+                        }
+
+                        override fun onCancelled(error: DatabaseError) {
+                            Toast.makeText(applicationContext, "Ups, da ist etwas schief gelaufen!",
+                                Toast.LENGTH_SHORT).show()
+                        }
+                    })
+                } else {
+                    Toast.makeText(applicationContext, "Gebe einen WG Namen ein!",
+                        Toast.LENGTH_SHORT).show()
+                }
             } else {
-                errorfield.text = "Bitte verwende keine Leerzeichen!"
+                Toast.makeText(applicationContext, "Bitte verwende keine Leerzeichen!",
+                    Toast.LENGTH_SHORT).show()
             }
         }
-
         popup.show()
     }
 }
