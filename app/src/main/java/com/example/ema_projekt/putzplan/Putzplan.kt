0d29ffaa6aac3fe8464d489a0a8c0fee90dc73c8
Jannis Bressan <jannisbressan@web.de>
2023-01-12 15:24:17 +0100

    idk

--- app/src/main/java/com/example/ema_projekt/putzplan/Putzplan.kt
+++ app/src/main/java/com/example/ema_projekt/putzplan/Putzplan.kt
@@ -3,10 +3,13 @@ package com.example.ema_projekt.putzplan
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.ema_projekt.R
+import androidx.appcompat.app.AppCompatDelegate
 
 class Putzplan : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_putzplan)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
     }
 }
