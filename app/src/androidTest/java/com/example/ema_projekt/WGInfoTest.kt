147896dc5876ebc7865c436e4d4ee3dc690e8da8
Yannick12365 <mcmasterdebabo@gmail.com>
2023-02-13 13:39:21 +0100

    add wginfo test

--- app/src/androidTest/java/com/example/ema_projekt/WGInfoTest.kt
+++ app/src/androidTest/java/com/example/ema_projekt/WGInfoTest.kt
@@ -2,8 +2,11 @@ package com.example.ema_projekt
 
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.ema_projekt.vorratskammer.VorratskammerActivity
 import com.example.ema_projekt.wginfo.WGInfoActivity
@@ -22,11 +25,23 @@ class WGInfoTest {
     fun addWGInfoTextCheck(){
         ActivityScenario.launch(WGInfoActivity::class.java)
         Thread.sleep(500)
-
+        onView(withId(R.id.button_hinzufuegen)).perform(click())
+        Thread.sleep(500)
+        onView(withId(R.id.editText_wginfotext)).perform(typeText("Ich bin ein WG Info Text aus dem Unit Test :)"))
+        Thread.sleep(500)
+        onView(withId(R.id.button_wginfotext_bestaetigen)).perform(click())
+        Thread.sleep(500)
     }
 
     @Test
     fun editWGInfoTextCheck(){
-
+        ActivityScenario.launch(WGInfoActivity::class.java)
+        Thread.sleep(500)
+        onView(withId(R.id.button_bearbeiten)).perform(click())
+        Thread.sleep(500)
+        onView(withId(R.id.editText_wginfotext)).perform(replaceText("Ich bin ein bearbeiteter WG Info Text aus dem Unit Test :)"))
+        Thread.sleep(500)
+        onView(withId(R.id.button_wginfotext_bestaetigen)).perform(click())
+        Thread.sleep(500)
     }
 }
